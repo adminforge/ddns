9b544b4e8a85aedddd8089b61c7686bb680eab7f
Ruben D <ruben@winterrific.net>
2025-08-30 21:40:10 +0200

    feat: add option for CARBON Server as ENV Variable

--- docker/powerdns/entrypoint.sh
+++ docker/powerdns/entrypoint.sh
@@ -4,4 +4,12 @@ CONFIG_FILE=/etc/powerdns/pdns.conf
 
 sed -i 's/{{PDNS_REMOTE_HTTP_HOST}}/'"${PDNS_REMOTE_HTTP_HOST}"'/g' ${CONFIG_FILE}
 
+if [ "${PDNS_CARBON_SERVER}" ]; then
+  echo "carbon-server=${PDNS_CARBON_SERVER}" >> "${CONFIG_FILE}"
+fi
+
+if [ "${PDNS_CARBON_OURNAME}" ]; then
+  echo "carbon-ourname=${PDNS_CARBON_OURNAME}" >> "${CONFIG_FILE}"
+fi
+
 exec "$@"
